728fbeff8e42adab1d83d82932b20dc3929a78e4
Benjamin D Adams <badams888@hotmail.com>
2020-05-24 21:56:12 -0400

    mostly working

--- mystery_words.py
+++ mystery_words.py
@@ -1,52 +1,88 @@
-# access words.txt 
-#  randomly select a word
-
-#  display as empty spaces,
-
-#  tell user how many letters
-#     if wrong -remove a possible guess (from 8)
-#         if # of guesses = 0, player loses the game
-# if correct, fill in letter
-# guess a letter
-# choose difficulty level
 
 import random
+
 def get_word():
     with open("words.txt") as words_pulled:
         words = words_pulled.readlines()
         return(random.choice(words))
-# print(get_word())
 
 def format_word():
     word_bank = []
-    working_word_bank = []
     obtained_word = get_word()
+    lower_obtained_word = obtained_word.lower()
     all_letters = "abcdefghijklmnopqrstuvwxyz"
-    for char in obtained_word:
-        
+    for char in lower_obtained_word:
         if char in all_letters:
-            word_bank.append(char)
-            # working_word_bank.append("_")
+            word_bank.append(char)        
     print(obtained_word)
     print(word_bank)
-    print(word_bank[2])
-    print(working_word_bank)
-    return word_bank, working_word_bank
-
+    return word_bank
 
 def guess_input():
-    guess = input("Guess a Letter!")
+    guess = input("Guess a Letter! ")
     return guess
+def display_right_guesses(letter, guesses):
+    if letter in guesses:
+        return letter
+    else:
+        return "_"
 
-# def check_guess():
-#     format_word()
-   
-    
+def check_guess(letter, word):
+    if letter in word:
+        return True
+    else:
+        return False
+
+    # THIS IS THE WORKING RUN GAME FUNCTION
+# def run_game():
+#     word = format_word()
+#     print(word)
+#     guess_count = 0
+#     current_guesses = []
+#     output = []
+#     letter = guess_input()
+#     current_guesses.append(letter)
+#     check = check_guess(letter, word)
+#     for letter in word:
+#         output.append(display_right_guesses(letter, current_guesses))
+#     if check == True:
+#         print("Letter is in word!")
+#     else:
+#             print("Sorry, guess again!")
+#             guess_count += 1
+#     print(current_guesses)
+#     print(output)
 
 def run_game():
-    format_word()
-    guess_count = 0
-    guess_input()
-    print (guess_input)
+    word = format_word()
+    print(word)
+    guess_count = 8
+    current_guesses = []
+    output = []
+
+
+    while guess_count !=0:
+        if output != word:
+            letter = guess_input()
+            current_guesses.append(letter)
+            check = check_guess(letter, word)
+            for letter in word:
+                output.append(display_right_guesses(letter, current_guesses))
+            print(output)
+            if output == word:
+                print("You guessed the word!!")
+                answer = input("Would you like to play again? (Y/N) ")
+                if answer == "Y":
+                    run_game()
+                else:    
+                    raise SystemExit
+            output = []
+            if check == True:
+                print("Letter is in word!")
+
+            elif check == False:
+                guess_count -= 1
+                print("Sorry, guess again!")
+                print(f"You have {guess_count} guesses remaining") 
 
 run_game()
